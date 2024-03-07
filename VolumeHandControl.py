412f6618133d8087360b16318a4b0da7cc33be01
annie-hola <anhanhanh151199@gmail.com>
2024-03-07 23:24:38 +0700

    main

---
+++ VolumeHandControl.py
@@ -0,0 +1,8 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
