cf78865f2a06b24d25775ca8cc3e794b250e1818
annie-hola <anhanhanh151199@gmail.com>
2024-03-07 23:25:47 +0700

    requirements

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -6,3 +6,12 @@ import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+################################
+wCam, hCam = 640, 480
+################################
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
